ade4850cddf9a0e234e0140448570a0ca80062fb
Daniel Lorenzo Reyes <dlorenzo@c3r9s5.42malaga.com>
2025-02-06 17:36:04 +0100

    v0.1 ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,17 @@
 /*   By: dlorenzo <dlorenzo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:58:46 by dlorenzo          #+#    #+#             */
-/*   Updated: 2025/02/05 17:31:05 by dlorenzo         ###   ########.fr       */
+/*   Updated: 2025/02/06 17:32:24 by dlorenzo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "ft_printf_utils.c"
+#include <stdio.h> // For printf --- DELETE ME;
 
 int	ft_printf(const char *data, ...)
 {
-	char	letter;
 
-	letter = data[0];
-	ft_putchar_fd(letter, 1);
-	return (1);
+	ft_putchar_fd((char)data[0], 1);
+	return (ft_strlen(data));
 }
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: dlorenzo <dlorenzo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:56:24 by dlorenzo          #+#    #+#             */
-/*   Updated: 2025/02/05 16:50:23 by dlorenzo         ###   ########.fr       */
+/*   Updated: 2025/02/06 16:10:18 by dlorenzo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,23 +36,21 @@ int	main(void)
 	int			ft_print_numchar;
 
 	printf("\033[35m---[Hello Main]---\033[0m\n"); // Magenta text
-	printf("   Test String used: '%s'\n", str); // Normal white text
+	printf("   Test String used: '%s'\n", str);
 
     printf("\033[32m   +++[printf]+++\033[0m\n"); // Green text
-	printf("\033[33m"); // Start Yellow
+	printf("\033[33m"); // Yellow text
 	print_numchar = real_printf(str);
 	printf("\nNumber of print chars: '%u'\n", print_numchar);
-    printf("\033[0m"); // Back to white
+    printf("\033[0m"); // White text
 
     printf("\033[34m   ***[ft_printf]***\033[0m\n");  // Blue text
-	printf("\033[36m"); // Start Cyan
+	printf("\033[36m"); // Cyan text
 	ft_print_numchar = fake_printf(str);
 	printf("\nNumber of ft_print chars: '%u'\n", ft_print_numchar);
-    printf("\033[0m"); // Back to white
+    printf("\033[0m"); // White text
 
 	printf("\033[31m---[Bye Main]---\033[0m\n");   // Red text
 
-	// real_printf(str);
-	// fake_printf(str);
 	return (0);
 }
